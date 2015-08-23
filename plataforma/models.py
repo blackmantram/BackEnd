e3df37c1fcc608eb039585b384f05dbe643c5049
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-08-23 09:40:43 -0500

    se agregó el campo para la imagen en los modelos cuestionario y  pregunta

--- plataforma/models.py
+++ plataforma/models.py
@@ -75,6 +75,7 @@ class RespuestaProblemaSolucion(models.Model):
    
 class Pregunta(models.Model):
   enunciado = models.CharField(max_length=200, null=True)
+  imagen = models.CharField(max_length=200, blank=True, null=True)
   tipo_pregunta = models.CharField(max_length=1,choices=(('U','UNICA RESPUESTA'),
     ('M','MULTIPLE RESPUESTA'),('L','LISTA')),default='U',
                                                   null=False, blank=False)
@@ -84,6 +85,7 @@ class Pregunta(models.Model):
 class Cuestionario(models.Model):
     titulo = models.CharField(max_length=200, null=True)
     descripcion =models.TextField(null=True)
+    imagen = models.CharField(max_length=200, blank=True, null=True)
     fecha = models.DateTimeField(auto_now=True, null=False)
     preguntas = models.ManyToManyField(Pregunta, through='CuestionarioPregunta')
    
