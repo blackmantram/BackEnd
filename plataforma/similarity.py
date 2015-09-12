57fbe210496751c49bb08d22e1c087fbfaca9ab4
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-09-12 10:51:34 -0500

    creación de funciones de similitudes provisionales

--- plataforma/similarity.py
+++ plataforma/similarity.py
@@ -32,12 +32,30 @@ def to_cuestionario(object_1, object_2):
              s=""
              for j in object_1[i]:
                respuesta_1 = OpcionesDeRespuesta.objects.get(pk=j[0])
-               s=s+respuesta_1+" , "
+               s=s+respuesta_1.respuesta+" , "
           else:
              respuesta_1 = OpcionesDeRespuesta.objects.get(pk=object_1[i][0])   
+             s = respuesta_1.respuesta
+          respuesta[pregunta.enunciado]=s
+        return respuesta    
 
-        respuesta[pregunta]=respuesta_1
-        return respuesta        
+#{1: (4,1) , 2: [(3,3) , (2,2) ],3: (6,1) }
+def similarity(o1, o2):
+    funciones = {1: s2, 2: s1, 3: s2}
+    s=0;
+    n=0;
+    for pregunta in o1:
+      s = s + funciones[pregunta](o1[pregunta],o2[pregunta])
+      n=n+1;
+    return s/n;
+
+
+def s1(x, y):
+  return len(list(set(x)&set(y)))/len(x)
+
+def s2(x,y):
+  return 1-abs(x[1]-y[1])/4
+
 
 
 
---
+++ prueba.py
@@ -0,0 +1,3 @@
+from plataforma.similarity import *
+x={1: (4,1) , 2: [(3,3) ],3: (6,1) }
+print to_cuestionario(x,x)
