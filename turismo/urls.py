b41cb58d625ead890c59b45b6afe12c769ca9835
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-10-07 08:03:12 -0500

    cambio a POST en ruta afinidad

--- turismo/urls.py
+++ turismo/urls.py
@@ -30,7 +30,7 @@ urlpatterns = patterns('',
     url(r'^cuestionarios/(?P<pk>[0-9]+)', views.CuestionarioRetrieve.as_view()), 
     url(r'^cuestionarios', views.CuestionarioList.as_view()), 
     url(r'^afinidad/detalle', views.AfinidadList.as_view({'get':'detail'})), 
-    url(r'^afinidad/', views.AfinidadList.as_view({'get':'list'})), 
+    url(r'^afinidad', views.AfinidadList.as_view({'post':'list'})), 
     url(r'^docs/', include('rest_framework_swagger.urls')), # url documentation
     
 
