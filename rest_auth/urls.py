dbe6c2affd3e032190a27d501e4a5c99127f992a
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2015-10-19 22:29:17 -0500

    reset password

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -8,14 +8,15 @@ from rest_auth.views import (
 urlpatterns = patterns(
     '',
     # URLs that do not require a session or valid token
-    url(r'^password/reset/$', PasswordResetView.as_view(),
+    url(r'^password/reset', PasswordResetView.as_view(),
         name='rest_password_reset'),
-    url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
-        name='rest_password_reset_confirm'),
-    url(r'^login/$', LoginView.as_view(), name='rest_login'),
+    url(r'^confirm/password/reset', PasswordResetConfirmView.as_view()),
+   # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
+    url(r'^login', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
-    url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
+    url(r'^logout', LogoutView.as_view(), name='rest_logout'),
     url(r'^user/$', UserDetailsView.as_view(), name='rest_user_details'),
-    url(r'^password/change/$', PasswordChangeView.as_view(),
+    url(r'^password/change', PasswordChangeView.as_view(),
         name='rest_password_change'),
 )
